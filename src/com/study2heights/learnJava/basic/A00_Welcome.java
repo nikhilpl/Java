1ba0f3f57be7426aad558b81f48a43c0b4716f48
NIKHIL P L <nikhil.pl.email@gmail.com>
2017-10-06 21:06:41 +0530

    October 06, 2017 09:06 PM
    Start

---
+++ src/com/study2heights/learnJava/basic/A00_Welcome.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.study2heights.learnJava.basic;
+
+//Welcome programming
+public class A00_Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming.... :-)");
+
+	}
+
+}
